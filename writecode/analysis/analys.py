97b620dd0f02d590c1dedb68fe9ed69be8b8dc7c
kangjian <kangjian@52tt.com>
2019-07-12 14:07:38 +0800

    --新增：分析系统函数的脚本，转换成json的格式

---
+++ writecode/analysis/analys.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import analysisstring
+import sys
+import os
+
+work_path = "/Users/animenzzz/GitCode/Confuse/writecode/analysis"
+
+file_path1 = f"{work_path}/res_orignFunc/uikit_class_func.txt"
+file_json_path1 = f"{work_path}/res_analysisedFunc/uikit_class_func.txt"
+
+
+file_path2 = f"{work_path}/res_orignFunc/uikit_class_instancetype_func.txt"
+file_json_path2 = f"{work_path}/res_AnalysisedFunc/uikit_class_instancetype_func.txt"
+
+file_path3 = f"{work_path}/res_orignFunc/uikit_member_func.txt"
+file_json_path3 = f"{work_path}/res_analysisedFunc/uikit_member_func.txt"
+
+file_path4 = f"{work_path}/res_orignFunc/uikit_member_instancetype_func.txt"
+file_json_path4 = f"{work_path}/res_analysisedFunc/uikit_member_instancetype_func.txt"
+
+
+if os.path.exists(file_json_path1):
+    os.remove(file_json_path1)
+
+if os.path.exists(file_json_path2):
+    os.remove(file_json_path2)
+
+if os.path.exists(file_json_path3):
+    os.remove(file_json_path3)
+
+if os.path.exists(file_json_path4):
+    os.remove(file_json_path4)
+
+for line in open(file_path1):
+    analysisstring.func(f'{line}',file_json_path1)
+
+for line in open(file_path2):
+    analysisstring.func(f'{line}',file_json_path2)
+
+for line in open(file_path3):
+    analysisstring.func(f'{line}',file_json_path3)
+
+for line in open(file_path4):
+    analysisstring.func(f'{line}',file_json_path4)
---
+++ writecode/analysis/analysisstring.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import re
+import json
+def func(funcstring,jsonoutputfile):
+
+    #获取参数类型，返回值类型
+    matchObj = re.findall('\((.*?)\)', funcstring)
+    resultDic = {}
+    resultDic["params"] = {}
+
+    if matchObj:
+        resultDic["returntype"] = matchObj[0]
+        for num in range(1,len(matchObj)): 
+            resultDic["params"] = matchObj[1:len(matchObj)]
+    
+    #获取函数名，参数描述
+    descrip = re.sub(u'\((.*?)\)', "",funcstring)
+    list1 = descrip.split(' ')
+    descriparr = []
+    if descrip:
+        index = str(list1[1]).find(":")
+        resultDic["funcname"] = str(list1[1])[0:index]
+        for num in range(2,len(list1)):
+            tmp = str(list1[num])
+            index = tmp.find(":")
+            tmp1 = tmp[0:index]
+            descriparr.append(tmp[0:index])
+        resultDic["descrip"] = descriparr
+    
+    jsonfile = open(jsonoutputfile,'a+')
+    jsonfile.writelines(json.dumps(resultDic)+'\n')
+    jsonfile.close()
